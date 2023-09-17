016a7f61a4f21a6d4978efa129ff0e2fd7e97cbd
Jonas Linux Mint <jonasmkunzler@gmail.com>
2023-09-17 00:22:31 -0300

    Novo build

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { generateAICompletionRoute } from './routes/generate-ai-completion'
 const app = fastify()
 
 app.register(fastifyCors, {
-  origin: '*', // UI url
+  origin: '*', // UI urll
 })
 
 app.register(getAllPromptsRoute)
